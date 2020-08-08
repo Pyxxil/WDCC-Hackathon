8970b7fda54370fd9f530468658bc4f1b3775642
Luicery <56919859+Luicery@users.noreply.github.com>
2020-08-09 09:51:32 +1200

    pushing to ivan branch

--- pages/form.js
+++ pages/form.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState, useEffect} from "react"
 import Layout from '../components/layout'
 import { useFetchUser } from '../lib/user'
@@ -33,44 +32,3 @@ function Form(props) {
   )
 }
 export default Form
-=======
-import React, { useState, useEffect } from "react";
-function Form(props) {
-  const [state, setState] = useState({});
-  function handleChange(event) {
-    event.persist();
-    setState((_state) => ({
-      ..._state,
-      [event.target.id]: event.target.value,
-    }));
-  }
-  function handleClick(e) {
-    console.log("asdasd");
-  }
-  return (
-    <div>
-      <form id="forUser">
-        <span>username</span>
-        <input onChange={handleChange} id="Username"></input>
-        <br />
-        <span>password</span>
-        <input onChange={handleChange} id="Password"></input>
-        <br />
-        <span>year</span>
-        <input onChange={handleChange} id="Year"></input>
-        <br />
-        <span>bachelor</span>
-        <input onChange={handleChange} id="Bachelor"></input>
-        <br />
-        <span>admin</span>
-        <input onChange={handleChange} id="Admin" type="checkbox"></input>
-        <br />
-        <input onChange={handleChange}></input>
-        <br />
-        <input type="submit" onClick={handleClick}></input>
-      </form>
-    </div>
-  );
-}
-export default Form;
->>>>>>> 0b0f94678c8c6cd5b9967fe120be532a2cbc12fe
