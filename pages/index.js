8c674e1b228271d400d4beec2f7f21b1a77c69bb
Josh Hill <josh.hill189@gmail.com>
2020-08-08 16:48:31 +1200

    Add Goals Endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,104 +1,142 @@
-import fetch from 'node-fetch'
-import Layout from '../components/layout'
-import { useFetchUser } from '../lib/user'
-import config from '../lib/config'
-import ProfileCard from '../components/ProfileCard'
-import Link from 'next/link'
-const LinkA = ({ children, href }) =>
+import fetch from "node-fetch";
+import Layout from "../components/layout";
+import { useFetchUser } from "../lib/user";
+import config from "../lib/config";
+import ProfileCard from "../components/ProfileCard";
+import Link from "next/link";
+const LinkA = ({ children, href }) => (
   <Link href={href}>
-    <a className='pl-4 block pr-4 underline hover:text-white'>{children}</a>
+    <a className="pl-4 block pr-4 underline hover:text-white">{children}</a>
   </Link>
+);
 
-  
-
-function Home () {
+function Home() {
   // set required to true to force the page to require login.
-  const { user, loading } = useFetchUser({ required: false })
+  const { user, loading } = useFetchUser({ required: false });
 
   const logEvent = async (type, value) => {
     const event = {
       name: user.nickname,
       type: type,
-      value: value
+      value: value,
       // date: added server side so we can't lie
-    }
+    };
+
     await fetch(`${config.HOST}/api/events`, {
-      method: 'post',
-      body: JSON.stringify(event)
-    })
+      method: "post",
+      body: JSON.stringify(event),
+    });
+
+    if (user) {
+      await fetch(`${config.HOST}/api/goals?user_id=${user.nickname}`, {
+        method: "post",
+        body: JSON.stringify(event),
+      });
+    }
 
     // TODO handle error if event cannot be posted.
     // TODO display feedback if event is ok
-  }
+  };
 
   const handleClick = (e) => {
     // console.log(e.target)
-    logEvent('click', 1)
-  }
+    logEvent("click", 1);
+  };
   return (
     <Layout user={user} loading={loading}>
-
       {loading && <p>Loading login info...</p>}
       {!loading && !user && (
         <>
+          <section className="container-hero">
+            <div>
+              <h1>Timeline</h1>
+              <div className="Divider"></div>
+              <p>Start your path to success. </p>
+              <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                href="#"
+              >
+                Signup
+              </button>
+              <p>
+                <a href="#" class="LearnMore">
+                  {" "}
+                  Or view all events{" "}
+                </a>
+              </p>
+            </div>
+          </section>
 
-  
-  <section className="container-hero">
-    <div>
-      <h1>Timeline</h1>
-      <div className="Divider"></div>
-      <p>Start your path to success. </p>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" href="#">
-  Signup
-</button>
-<p><a href="#" class="LearnMore"> Or view all events </a></p>
-    </div>
-  </section>
+          <section className="container-about">
+            <div>
+              <h2>About</h2>
+              <div className="Divider"></div>
+              <p> This is our product!! </p>
+            </div>
+          </section>
 
-  <section className="container-about">
-  <div>
-      <h2>About</h2>
-      <div className="Divider"></div>
-      <p> This is our product!! </p>
-      
-    </div>
-  </section>
-   
-   <section className="container-features">
-     <div>
-       <h2>Features</h2>
-       <div className = "Divider"/>
-      </div>
-      <div className="container-features-grid">
-      <div><img src="/img/cal.svg"/><p><h4>Calendar:</h4> {/* add logo of calendar */} Keep track of your assignments and upcoming events.</p></div>
-      <div><img src="/img/profile.svg"/><p><h4>Personalized:</h4> {/* add logo of a tick */} Tell us your goals and let us personalize all the events for you. </p></div>
-      <div><img src="/img/info.svg"/><p><h4>Consolidated Information:</h4> {/* add logo of a tick */} Information at the touch of your fingers, curated by us. </p></div>
-      <div><img src="/img/event.svg"/><p><h4>Events:</h4> {/* add logo of a tick */} See relevant career workshops and club events. </p></div>
-      </div>
-       
-    </section>
+          <section className="container-features">
+            <div>
+              <h2>Features</h2>
+              <div className="Divider" />
+            </div>
+            <div className="container-features-grid">
+              <div>
+                <img src="/img/cal.svg" />
+                <p>
+                  <h4>Calendar:</h4> {/* add logo of calendar */} Keep track of
+                  your assignments and upcoming events.
+                </p>
+              </div>
+              <div>
+                <img src="/img/profile.svg" />
+                <p>
+                  <h4>Personalized:</h4> {/* add logo of a tick */} Tell us your
+                  goals and let us personalize all the events for you.{" "}
+                </p>
+              </div>
+              <div>
+                <img src="/img/info.svg" />
+                <p>
+                  <h4>Consolidated Information:</h4> {/* add logo of a tick */}{" "}
+                  Information at the touch of your fingers, curated by us.{" "}
+                </p>
+              </div>
+              <div>
+                <img src="/img/event.svg" />
+                <p>
+                  <h4>Events:</h4> {/* add logo of a tick */} See relevant
+                  career workshops and club events.{" "}
+                </p>
+              </div>
+            </div>
+          </section>
 
-    <footer>
-      <a href="#"  id = "TOC"> Terms of Conditions</a>
-
-    </footer>     
+          <footer>
+            <a href="#" id="TOC">
+              {" "}
+              Terms of Conditions
+            </a>
+          </footer>
         </>
-  )}
+      )}
 
       {/* After login see the information below */}
       {user && (
         <>
           <ProfileCard user={user}>
-            <button className='btn-blue' onClick={handleClick}>Event</button>
+            <button className="btn-blue" onClick={handleClick}>
+              Event
+            </button>
           </ProfileCard>
           <h2>Reports</h2>
-          <LinkA href='/report'>Report - useEffect</LinkA>
-          <LinkA href='/reportSSR'>Report - SSR</LinkA>
-          <LinkA href='/reportSWR'>Report - SWR</LinkA>
-
-        </>)}
+          <LinkA href="/report">Report - useEffect</LinkA>
+          <LinkA href="/reportSSR">Report - SSR</LinkA>
+          <LinkA href="/reportSWR">Report - SWR</LinkA>
+        </>
+      )}
     </Layout>
-  )
+  );
 }
 
-export default Home
+export default Home;
