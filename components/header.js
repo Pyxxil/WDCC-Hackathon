d99beedf62ce3464c0c4ad4ae78c92443680bcee
Josh Hill <josh.hill189@gmail.com>
2020-08-09 10:28:24 +1200

    Fix Merge Issues

--- components/header.js
+++ components/header.js
@@ -13,29 +13,18 @@ const MenuItem = ({ children, href }) => {
   );
 };
 
-
-function Header ({ user, loading }) {
+function Header({ user, loading }) {
   return (
-    <header className='bg-orange-300'>
-      <nav className='container flex items-center justify-between flex-wrap  py-2 '>
-        <div id='Brand' className='text-3xl text-black-700'>
-          WDCC Workshop Application
+    <header className="navbar">
+      <nav className="container flex items-center justify-between flex-wrap">
+        <div id="Brand" className="logo">
+          <MenuItem id="landing-page" href="/">
+            Timeline
+          </MenuItem>
         </div>
-<<<<<<< HEAD
         <ul className="flex">
-=======
-        <ul className=' flex   '>
-          <MenuItem href='/'>Home</MenuItem>
-          <MenuItem href="/form">MUser</MenuItem>
-          <MenuItem href="/formevent">MEvent</MenuItem>
-          <MenuItem href="/formclub">MClub</MenuItem>
-          <MenuItem href="/formgoal">MGoal</MenuItem>
-          <MenuItem href="/formjob">MJob</MenuItem>
-          <MenuItem href='/about'>About</MenuItem>
-          <MenuItem href="/calendar">Calendar</MenuItem>
-          <MenuItem href="/event">Event</MenuItem>
->>>>>>> 8970b7fda54370fd9f530468658bc4f1b3775642
           <MenuItem href="/events">Events</MenuItem>
+
           {!loading &&
             (user ? (
               <>
