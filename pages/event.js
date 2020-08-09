ed4b040c73ea99c4532a763a1889cf2f0ee8f185
Luicery <56919859+Luicery@users.noreply.github.com>
2020-08-09 16:31:42 +1200

    ASDJLANSDJK

--- pages/event.js
+++ pages/event.js
@@ -1,6 +1,40 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios"; //For later
+// import { useFetchUser } from '../lib/user'
+// function Event(props) {
+//   const {user, loading} = useFetchUser();
+//   const [date, setDate] = useState("");
+//   const [title, setTitle] = useState("");
+//   const [data, setData] = useState([]);
+//   // useEffect(() => {
+//   //   axios.get("/api/events").then()
+//   //   .catch((err) => console.log(err))
+//   // }, []) For events later
+//   //Mocked event
+//   useEffect(() => {
+//     setData({
+//       title: "Wow an Event",
+//       date: "1/01/01",
+//       description:
+//
+//     console.log(user)
+//   }, []);
+//   return (
+//     <div>
+//       <h2 id="title">
+//         {data.title} hosted by {data.club}
+//       </h2>
+//       <h2 id="date">{data.date}</h2>
+//       <div id="description">{data.description}</div>
+//     </div>
+//   );
+// }
+// export default Event;
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Layout from '../components/layout'
+import axios from "axios"; //For later
 import { useFetchUser } from '../lib/user'
 import Calendar from "react-calendar"
 import ShowMap from "./googlemaps.js"
@@ -12,11 +46,13 @@ function Event(props) {
   const [lngpos, setLngpos] = useState("")
   const [day, setDay] = useState("")
   useEffect(() => {
-    // axios.get("").then(res => {
-    //  setData(res.data)
-  // }).catch(console.warn())
     setDay(new Date(2020,1,1))
   },[])
+  const {user, loading} = useFetchUser();
+  const [date, setDate] = useState("");
+  const [title, setTitle] = useState("");
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     setData({
       "date": "20/01/20",
@@ -50,7 +86,23 @@ function Event(props) {
         </div>
       </div>
       </Layout>
+      title: "Wow an Event",
+      date: "1/01/01",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      club: "A club",
+    });
+    console.log(user)
+  }, []);
+  return (
+    <div>
+      <h2 id="title">
+        {data.title} hosted by {data.club}
+      </h2>
+      <h2 id="date">{data.date}</h2>
+      <div id="description">{data.description}</div>
     </div>
   )
+  );
 }
-export default Event
+export default Event;
