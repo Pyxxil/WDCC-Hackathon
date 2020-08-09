3f443796f155ee2a1dbd14936d64d4ca2f38276c
Luicery <56919859+Luicery@users.noreply.github.com>
2020-08-09 13:46:03 +1200

    To my branch first

--- pages/calendar.js
+++ pages/calendar.js
@@ -15,7 +15,6 @@ function Showcalendar(props) {
     const monthShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     month = monthNum[monthShort.indexOf(month)]
     const route = `/calendar/${day}/${month}/${year}`;
-    console.log(route)
   }
   return(
     <div>
--- pages/formclub.js
+++ pages/formclub.js
@@ -1,42 +1,30 @@
-import React, { useState, useEffect } from "react";
-import Layout from "../components/layout";
-import { useFetchUser } from "../lib/user";
+import React, {useState, useEffect} from "react"
+import Layout from '../components/layout'
+import { useFetchUser } from '../lib/user'
 
 function Form(props) {
-  const { user, loading } = useFetchUser();
+  const {user, loading} = useFetchUser();
   const [state, setState] = useState({});
   function handleChange(event) {
-    event.persist();
-    setState((_state) => ({
-      ..._state,
-      [event.target.id]: event.target.value,
-    }));
+    event.persist()
+    setState((_state) => ({ ..._state, [event.target.id]:event.target.value }));
   }
   function handleClick(e) {
-    axios
-      .post("/api/user", { state })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch(console.warn());
+    axios.post("/api/user", {state}).then(res => {
+      console.log(res)
+    }).catch(console.warn())
   }
-  return (
+  return(
     <div>
       <Layout user={user} loading={loading}>
         <form id="forUser">
-          <span>Name</span>
-          <input onChange={handleChange} id="Name"></input>
-          <br />
-          <span>Members</span>
-          <input onChange={handleChange} id="Members"></input>
-          <br />
-          <span>Leader</span>
-          <input onChange={handleChange} id="Leader"></input>
-          <br />
+          <span>Name</span><input onChange={handleChange} id="Name"></input><br/>
+          <span>Members</span><input onChange={handleChange} id="Members"></input><br/>
+          <span>Leader</span><input onChange={handleChange} id="Leader"></input><br/>
           <input type="submit" onClick={handleClick}></input>
         </form>
       </Layout>
     </div>
-  );
+  )
 }
-export default Form;
+export default Form
